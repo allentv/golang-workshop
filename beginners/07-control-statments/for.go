c2b541b2feea09bef36cdb6b9e730359404e44f3
Allen Thomas Varghese <allentv4u@gmail.com>
2019-03-16 21:42:18 -0400

    Add break and continue statements

--- beginners/07-control-statments/for.go
+++ beginners/07-control-statments/for.go
@@ -3,7 +3,38 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("1 .. 10 =>")
+	simple()
+
+	fmt.Println("\n\n1 .. 7 =>")
+	simpleBreak()
+
+	fmt.Println("\n\nOdd numbers =>")
+	simpleContinue()
+}
+
+func simple() {
 	for i := 1; i <= 10; i = i + 1 {
-		fmt.Println(i)
+		fmt.Print(i, " ")
+	}
+}
+
+// simpleBreak will print numbers from 1 to 7
+func simpleBreak() {
+	for i := 1; i <= 10; i = i + 1 {
+		if i == 8 {
+			break
+		}
+		fmt.Print(i, " ")
+	}
+}
+
+// simpleContinue will print only odd numbers
+func simpleContinue() {
+	for i := 1; i <= 10; i = i + 1 {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Print(i, " ")
 	}
 }
