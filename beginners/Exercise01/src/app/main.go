733489e06f771c537a586c1af11af280033018b5
Allen Thomas Varghese <allentv4u@gmail.com>
2019-03-21 21:34:47 +0000

    Add file based variation and use a separate package

--- beginners/Exercise01/src/app/main.go
+++ beginners/Exercise01/src/app/main.go
@@ -1,12 +1,24 @@
+// Reading data from the command line and then print out the summary
+
 package main
 
 import (
+	"app/utils"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"strings"
 )
+
+// cleanupInput runs all the validation and bleaching to be applied on the input string
+func cleanupInput(inputStr string) string {
+	if inputStr == "" {
+		log.Fatal("Input string not provided!")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	return strings.ToLower(inputStr)
+}
 
 func main() {
 	inputStr := flag.String("text", "", "Text block to parse")
@@ -14,67 +26,10 @@ func main() {
 
 	cleanedStr := cleanupInput(*inputStr)
 
-	vowels := findVowels(cleanedStr)
-	digits := findDigits(cleanedStr)
-	symbols := findSymbols(cleanedStr)
-	words := findWords(cleanedStr)
+	vowels := utils.FindVowels(cleanedStr)
+	digits := utils.FindDigits(cleanedStr)
+	symbols := utils.FindSymbols(cleanedStr)
+	words := utils.FindWords(cleanedStr)
 
-	printReport(vowels, digits, symbols, words)
-}
-
-// cleanupInput runs all the validation and bleaching to be applied on the input string
-func cleanupInput(inputStr string) string {
-	if inputStr == "" {
-		log.Fatal("Input string not provided!")
-		os.Exit(1)
-	}
-	return strings.ToLower(inputStr)
-}
-
-// findVowels will check if the character is one of a, e, i, o, u
-func findVowels(s string) int {
-	result := 0
-	for _, v := range s {
-		if strings.ContainsAny("aeiou", string(v)) {
-			result++
-		}
-	}
-	return result
-}
-
-// findDigits will check only for digits from 0 to 9
-func findDigits(s string) int {
-	result := 0
-	for _, v := range s {
-		if strings.ContainsAny("1234567890", string(v)) {
-			result++
-		}
-	}
-	return result
-}
-
-// findSymbols will count common symbols appearing on the number keys
-func findSymbols(s string) int {
-	result := 0
-	for _, v := range s {
-		if strings.ContainsAny("!@€£#$%^&*()", string(v)) {
-			result++
-		}
-	}
-	return result
-}
-
-// findWords will count the number of words without surrounding spaces
-func findWords(s string) int {
-	return len(strings.Fields(s))
-}
-
-// printReport will display a summary of results
-func printReport(vowels, digits, symbols, words int) {
-	fmt.Println("Analysis Summary")
-	fmt.Println("-------- -------")
-	fmt.Println("vowels  :", vowels)
-	fmt.Println("digits  :", digits)
-	fmt.Println("symbols :", symbols)
-	fmt.Println("words   :", words)
+	utils.PrintReport(vowels, digits, symbols, words)
 }
